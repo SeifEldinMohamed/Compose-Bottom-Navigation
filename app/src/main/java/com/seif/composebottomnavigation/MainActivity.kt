0581a8a7b60e62573b1c243eec8c16eb91513808
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-04-10 18:11:16 +0200

    themes and MainActivity.kt Added

---
+++ app/src/main/java/com/seif/composebottomnavigation/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.seif.composebottomnavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.seif.composebottomnavigation.ui.theme.ComposeBottomNavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBottomNavigationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
